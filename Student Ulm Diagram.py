6a0034e9aed486fda9a86456de62a43ffb0a5548
Krisjanis Ulmanis <vabolmilis@gmail.com>
2022-09-15 10:03:58 +0200

    Student added

--- Student Ulm Diagram.py
+++ Student Ulm Diagram.py
@@ -17,7 +17,29 @@ class Person:
     def eat(self):
         print(f"nom nom nom so tasty")
         return f"{self.name} has eaten"
-    def drink(self:)
+    def drink(self):
         print(f"now I am not thirsty!")
         return f"{self.name} drinks"    
+    
+class Student(Person):
+    
+    def __init__(self, school, grade, subjects_studied, credits):
+        super().__init__()
+        self.school = school
+        self.grade = grade
+        self.subjects_studied = subjects_studied
+        self.credits = credits
+        print(f"{self.__Person__.__name__} studies at {self.school} in the {self.grade} grade.")
+
+    def study(self):
+        return f"{self.__Person__.__name__} is busy not getting bitches."
+    
+    def complain(self):
+        return f"{self.__Person__.__name__} is complaining about {self.school}"
+
+#    def passExam(self, credits)
+
+    def failExam(self):
+        return f"{self.__Person__.__name__} failed his exam."
+
     
